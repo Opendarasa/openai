be25fff0f49432d2514ec4c1df1ead0ed4278f83
林怡雯 <Edwina@linyiwende-MacBook-Pro.local>
2017-05-12 02:08:03 +0800

    init20

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.get('/webhook/',function(req,res)
     }
     res.send('No Entry')
 })
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"new_thread",
+  "call_to_actions":[
+    {
+      "payload":"USER_DEFINED_PAYLOAD"
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=token"      
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
