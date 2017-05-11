a0108b3534b5fce83bb3d391916beeed93fc76e7
林怡雯 <Edwina@linyiwende-MacBook-Pro.local>
2017-05-11 18:12:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const expresss= require ('expresss')
+const bodyParser= require('body-parser')
+const request =require('request')
+
+const app=expresss
+
+app.set('port',(process.env.PORT||500))
+
+app.use(bodyParser.urlenconded({extended:false}))
+
+app.use(bodyParser.json())
+
+app.get('/',function(req,res)
+{
+    
+    res.send('Hello Youtube')
+})
+
+app.get('/webhook/',function(req,res)
+{
+    if(req.query['hub.verify_token']==='my_voice_is_my_password_verify_me')
+    {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('No Entry')
+})
+app.listen(app.get('port'),function()
+{
+    console.log('running on port',app.get('port'))
+})
+
+
