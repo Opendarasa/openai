f4f5dbb16e8c6355c3875b563722fd7d24e00537
林怡雯 <Edwina@linyiwende-MacBook-Pro.local>
2017-05-11 18:35:59 +0800

    init7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require ('express')
 const bodyParser= require('body-parser')
 const request =require('request')
 
-const app=express
+const app=express()
 
 app.set('port',(process.env.PORT||5000))
 
