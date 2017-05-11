80d924bcef8e24911fa9704e9a6de0e487b4a162
林怡雯 <Edwina@linyiwende-MacBook-Pro.local>
2017-05-11 18:41:29 +0800

    init8

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express()
 
 app.set('port',(process.env.PORT||5000))
 
-app.use(bodyParser.urlenconded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(bodyParser.json())
 
