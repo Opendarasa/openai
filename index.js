8b2ceff3aef57dd6a434e894e9b017b3022d14d8
林怡雯 <Edwina@linyiwende-MacBook-Pro.local>
2017-05-16 14:00:26 +0800

    bug2

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ function scheduleHtml(senderId, message)
     
     
     rule.dayOfWeek=[0,new schedule.Range(0,6)];
-    rule.hour=8;
-    rule.minute=0;
+    rule.hour=5;
+    rule.minute=53;
     
     
         
